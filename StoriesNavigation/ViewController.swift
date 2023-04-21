4fa7c656cc91a5cb5692c283a6e24fa5a96a793f
ts-sam0807 <ts.sam0807@gmail.com>
2023-04-22 00:33:44 +0800

    Create Project

---
+++ StoriesNavigation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StoriesNavigation
+//
+//  Created by Ts SaM on 21/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func unwindtoRoot(seque: UIStoryboardSegue) {
+    
+  }
+
+}
+
